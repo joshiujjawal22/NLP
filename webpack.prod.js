8fa5577223957d449a90113eb345b7d2870e3c60
UJJAWAL JOSHI <joshiujjawal22@gmail.com>
2020-04-19 08:42:36 +0530

    Add files via upload

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,31 +2,25 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 
-
 module.exports = {
     mode: 'production',
-    entry: './src/client/index.js',
-     output: {
-      libraryTarget: 'var',
-      library: 'Client'
-    },
+	entry: './src/client/index.js',
+	devtool: 'source-map',
     module: {
         rules: [
-                {
-            test: '/\.js$/',
-            exclude: /node_modules/,
-            loader: "babel-loader"
-                },
             {
-                test: /\.scss$/,
-                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
-                }
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
         ]
     },
     plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/client/views/index.html",
-            filename: "./index.html"
-        }),
+    new HtmlWebPackPlugin(
+    {
+        template: "./src/client/views/index.html",
+        filename: "./index.html",
+    })
     ]
+
 }
