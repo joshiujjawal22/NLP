6161b03575d1fbe25c50619867a6ab4b1ca15bc6
UJJAWAL JOSHI <joshiujjawal22@gmail.com>
2020-04-19 08:56:49 +0530

    Add files via upload
    Added service worker

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,25 +2,32 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 
+
 module.exports = {
     mode: 'production',
-	entry: './src/client/index.js',
-	devtool: 'source-map',
+    entry: './src/client/index.js',
+     output: {
+      libraryTarget: 'var',
+      library: 'Client'
+    },
     module: {
         rules: [
+                {
+            test: '/\.js$/',
+            exclude: /node_modules/,
+            loader: "babel-loader"
+                },
             {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+                }
         ]
     },
     plugins: [
-    new HtmlWebPackPlugin(
-    {
-        template: "./src/client/views/index.html",
-        filename: "./index.html",
-    }),
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html"
+        }),
         new WorkboxPlugin.GenerateSW()
     ]
 
